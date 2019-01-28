7116a99713043b9381ca67cd01ba00946d0fe190
Tuan Vu <tuanvuf4@gmail.com>
2019-01-28 18:12:10 +0700

    update

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -5,51 +5,51 @@ export class Header extends Component {
 	render() {
 		return (
 			<div className="header">
-				<nav class="navbar navbar-default">
-					<div class="container-fluid">
+				<nav className="navbar navbar-default">
+					<div className="container-fluid">
 
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
+						<div className="navbar-header">
+							<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span className="sr-only">Toggle navigation</span>
+								<span className="icon-bar"></span>
+								<span className="icon-bar"></span>
+								<span className="icon-bar"></span>
 							</button>
-							<Link class="navbar-brand" to="#"><img src={ process.env.PUBLIC_URL + '/assets/corporate/img/logos/logo-shop-red.png' } /></Link>
+							<Link className="navbar-brand" to="#"><img src={ process.env.PUBLIC_URL + '/assets/corporate/img/logos/logo-shop-red.png' } alt="" /></Link>
 						</div>
 
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								<li class="active"><Link to="#">Link <span class="sr-only">(current)</span></Link></li>
+						<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul className="nav navbar-nav">
+								<li className="active"><Link to="#">Link <span className="sr-only">(current)</span></Link></li>
 								<li><Link to="#">Link</Link></li>
-								<li class="dropdown">
-									<Link to="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></Link>
-									<ul class="dropdown-menu">
+								<li className="dropdown">
+									<Link to="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></Link>
+									<ul className="dropdown-menu">
 										<li><Link to="#">Action</Link></li>
 										<li><Link to="#">Another action</Link></li>
 										<li><Link to="#">Something else here</Link></li>
-										<li role="separator" class="divider"></li>
+										<li role="separator" className="divider"></li>
 										<li><Link to="#">Separated link</Link></li>
 									</ul>
 								</li>
 							</ul>
 
-							<form class="navbar-form navbar-left">
-								<div class="form-group">
-									<input type="text" class="form-control" placeholder="Search" />
+							<form className="navbar-form navbar-left">
+								<div className="form-group">
+									<input type="text" className="form-control" placeholder="Search" />
 								</div>
-								<button type="submit" class="btn btn-default">Submit</button>
+								<button type="submit" className="btn btn-default">Submit</button>
 							</form>
 
-							<ul class="nav navbar-nav navbar-right">
+							<ul className="nav navbar-nav navbar-right">
 								<li><Link to="#">Link</Link></li>
-								<li class="dropdown">
-									<Link to="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></Link>
-									<ul class="dropdown-menu">
+								<li className="dropdown">
+									<Link to="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></Link>
+									<ul className="dropdown-menu">
 										<li><Link to="#">Action</Link></li>
 										<li><Link to="#">Another action</Link></li>
 										<li><Link to="#">Something else here</Link></li>
-										<li role="separator" class="divider"></li>
+										<li role="separator" className="divider"></li>
 										<li><Link to="#">Separated link</Link></li>
 									</ul>
 								</li>
