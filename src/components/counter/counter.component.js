79dadfc66f5c502db4ca63c8dbf7d227f23c1c93
Tuan Vu <tuanvuf4@gmail.com>
2019-03-06 15:41:17 +0700

    update

--- src/components/counter/counter.component.js
+++ src/components/counter/counter.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { store } from "../../App";
 import { DECREASE, INCREASE } from "../../actions/actions";
 import './counter.component.scss';
 
