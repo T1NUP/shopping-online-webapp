0b76475b18bf6dd9ebded2967cf9e4a99125f01b
unknown <hoangnttdata@gmail.com>
2019-04-10 08:35:52 +0700

    install lib

---
+++ src/containers/new-arrivals/new-arrivals.container.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import NewArrivals from './new-arrivals.container';
+
+describe('NewArrivals', () => {
+    it('Test example', () => {
+        // const wrapper = shallow(
+        //     <NewArrivals />
+        // );
+        expect(true).toBe(true);
+    });
+});
